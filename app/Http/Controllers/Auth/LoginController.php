25e5668b98f87e32d0c5b4508b2678f43aa1fac1
Edd-brother <eddbrothersong@gmail.com>
2021-04-06 03:23:52 -0700

    update:hash password

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,8 +8,13 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
 
 use Auth;
+
+use function PHPUnit\Framework\isEmpty;
+use function PHPUnit\Framework\isNull;
 
 class LoginController extends Controller
 {
@@ -46,64 +51,70 @@ class LoginController extends Controller
     protected function attemptLogin(Request $request)
     {
         $user = User::where('email', $request->email)
-        ->where('password', $request->password)
-        ->first();
+            ->where('password', $request->password)
+            ->first();
 
         // var_dump($user->password);die;
 
 
         $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
-        $user = User::where($fieldType, '=', $input['username'])->where('password', '=', $input['password'])->first();
+        $user = User::where($fieldType, '=', $input['username'])->first();
 
-        if($user)
-        {
+        if ($user) {
             auth()->loginUsingId($user->id);
             return $this->sendLoginResponse($request);
             //return redirect()->route('home');
-        }else{
+        } else {
             $validator->errors()->add('username', 'These credentials do not match our records.');
             return redirect()->route('login')->withErrors($validator)->withInput();
         }
 
-        if(!isset($user))
-        {
+        if (!isset($user)) {
             return false;
         }
 
         Auth::login($user);
 
         return true;
-
     }
 
 
-    protected function login(Request $request){
+    protected function login(Request $request)
+    {
         $input = $request->all();
-  
+
+        // var_dump(Hash::check("123123123", '$2y$10$kBAPK4eQdspCJOzH/yG1BegMFAvwGcZ989Fqn8XOQshkNWVm.Jx6i'));
+        // die;
+
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'password' => 'required',
         ]);
-  
-        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'login';
-        $user = User::where($fieldType, '=', $input['username'])->where('password', '=', $input['password'])->first();
 
-        if($user)
-        {
-            auth()->loginUsingId($user->id);
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'login';
+        $user = User::where($fieldType, '=', $input['username'])->first();
+        // var_dump(Hash::check($request['password'], $user->));die;
+        // var_dump($user->password);die;
+        if (Hash::check($input['password'], $user->password)) {
+
+            // die;
+            $request->session()->regenerate();
+            if (session_id() != $user->last_session) {
+                $this->logout();
+                return true;
+            }
+
+            auth()->loginUsingId($user->id, true);
+            // var_dump(session_id());
+            // die;
+
+            $user->last_session = session_id();
+            $user->save();
             return $this->sendLoginResponse($request);
             //return redirect()->route('home');
-        }else{
+        } else {
             $validator->errors()->add('username', 'These credentials do not match our records.');
             return redirect()->route('login')->withErrors($validator)->withInput();
-        }
-    }
-
-    public function authenticate()
-    {
-        if (Auth::attempt(['email' => $email, 'password' => $password])) {
-            // Authentication passed...
-            return redirect()->intended('dashboard');
         }
     }
 
@@ -111,5 +122,22 @@ class LoginController extends Controller
     {
         Auth::logout();
         return redirect('login');
+    }
+
+    public function authenticate()
+    {
+        if (Auth::attempt(['email' => $email, 'password' => $password], true)) {
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+
+        $this->clearLoginAttempts($request);
+
+        return $this->authenticated($request, $this->guard()->user())
+            ?: redirect()->intended($this->redirectPath());
     }
 }
