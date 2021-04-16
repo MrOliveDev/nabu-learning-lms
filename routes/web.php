9f7420b2fb3f07d6dbd3f3f8fcf8e4b930110be3
Edd-brother <eddbrothersong@gmail.com>
2021-04-15 22:01:36 -0700

    update:menu sequence

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => ['auth' ,'checksinglesession'], 'prefix' => ''], f
     Route::get('session', '\App\Http\Controllers\SessionController@index')->name('session');
     Route::get('lesson', '\App\Http\Controllers\LessonController@index')->name('lesson');
     Route::get('admin/dash', '\App\Http\Controllers\admin\DashController@index')->name('admin.dash');
-    Route::get('dash', '\App\Http\Controllers\common\DashController@index')->name('common.dash');
+    Route::get('dash', '\App\Http\Controllers\common\DashController@index')->name('dash');
     Route::get('temp', '\App\Http\Controllers\TempController@index')->name('temp');
 });
 
