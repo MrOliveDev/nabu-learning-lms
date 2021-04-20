40de153f34cb6f53fc4c66b7aff363ff93794463
Edd-brother <eddbrothersong@gmail.com>
2021-04-20 10:07:18 -0700

    update: feedback 3, 6 check

---
+++ app/Models/TemplateModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TemplateModel extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'alpha_id', 'name', 'code', 'id_creator', 'style', 'published'
+    ];
+
+    protected $table = 'tb_template_html5_edit';
+}
