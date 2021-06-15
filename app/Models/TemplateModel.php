01e0a151c1646140593fffcd41c27a1b8e406ff6
Edd-brother <eddbrothersong@gmail.com>
2021-06-15 13:53:55 -0700

    update template page first

--- app/Models/TemplateModel.php
+++ app/Models/TemplateModel.php
@@ -10,7 +10,7 @@ class TemplateModel extends Model
     use HasFactory;
 
     protected $fillable = [
-        'alpha_id', 'name', 'code', 'id_creator', 'style', 'published'
+        'alpha_id', 'name', 'code', 'id_creator', 'style', 'published', 'creation_date'
     ];
 
     protected $table = 'tb_template_html5_edit';
