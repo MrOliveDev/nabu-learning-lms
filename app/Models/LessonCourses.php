172140f7915c55b72416dc6345e47dd8a7f311b7
OliveSonDev <olivesondev@gmail.com>
2021-07-21 09:01:33 +0900

    update: html5_player_api & getReportData initial finish

--- app/Models/LessonCourses.php
+++ app/Models/LessonCourses.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ReportTemplateModel extends Model
+class LessonCourses extends Model
 {
     use HasFactory;
 
