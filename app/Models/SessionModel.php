810a36a58a54a0b5349cf09ed4fb7a12ac8dae98
Edd-brother <ninjakostiapop@gmail.com>
2021-08-23 03:53:23 +1000

    fix the admin dash page bug

--- app/Models/SessionModel.php
+++ app/Models/SessionModel.php
@@ -247,6 +247,19 @@ class SessionModel extends Model
 
     public function scopeGetParticipantListFromSessionForDash($query, $participant_data, $session_id)
     {
+        DB::connection('mysql_reports')->unprepared('CREATE TABLE IF NOT EXISTS `tb_screen_optim_'.$session_id.'` (
+            `id_screen_optim` int(11) NOT NULL AUTO_INCREMENT,
+            `id_fabrique_screen_optim` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+            `id_curso_screen_optim` int(11) NOT NULL,
+            `id_user_screen_optim` int(11) NOT NULL,
+            `progress_details_screen_optim` text COLLATE utf8_unicode_ci NOT NULL,
+            `progress_screen_optim` float(5,2) NOT NULL,
+            `last_date_screen_optim` datetime NOT NULL,
+            `first_eval_id_screen_optim` int(11) NOT NULL,
+            `last_eval_id_screen_optim` int(11) NOT NULL,
+            PRIMARY KEY (id_screen_optim) 
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+            ');   
         $groupData = array();
         $studentData = array();
         $teacherData = array();
