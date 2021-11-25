8aec1de96181ffec39d53e2bf8faf0d5f798af92
OliveSonDev <olivesondev@gmail.com>
2021-11-26 01:04:36 +0900

    update: add sessionId to progression api

--- html5_player_api/app/userCourse/userProgression.php
+++ html5_player_api/app/userCourse/userProgression.php
@@ -63,8 +63,6 @@
             } // eo constructor
         } // eo openModel class
 
-        $openModel  = new openModel;
-
         /**
          * GET historic datas from DATABASE
          */
@@ -74,8 +72,28 @@
         $cursoId = $curso_result[0]['id'];
         $return['cursoId']       = $cursoId;
 
+        if($sessionId){
+            $tableName = "tb_screen_optim_" . $sessionId;
+            $openModel = new openModel(DB_HISTORIC_DSN);
+            $createSql = "CREATE TABLE IF NOT EXISTS `{$tableName}` ("
+            . "`id_screen_optim` int(11) NOT NULL AUTO_INCREMENT,"
+            . "`id_fabrique_screen_optim` varchar(10) COLLATE utf8_unicode_ci NOT NULL,"
+            . "`id_curso_screen_optim` int(11) NOT NULL,"
+            . "`id_user_screen_optim` int(11) NOT NULL,"
+            . "`progress_details_screen_optim` text COLLATE utf8_unicode_ci NOT NULL,"
+            . "`progress_screen_optim` float(5,2) NOT NULL,"
+            . "`last_date_screen_optim` datetime NOT NULL,"
+            . "`first_eval_id_screen_optim` int(11) NOT NULL,"
+            . "`last_eval_id_screen_optim` int(11) NOT NULL,"
+            . "PRIMARY KEY (id_screen_optim) "
+            . ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+            $openModel->query( $createSql );
+        } else {
+            $openModel  = new openModel;
+            $tableName = "tb_screen_optim";
+        }
         $sql         = "SELECT `id_screen_optim`, `id_fabrique_screen_optim`, `id_curso_screen_optim`, `id_user_screen_optim`, `progress_details_screen_optim` ";
-        $sql        .= "FROM `tb_screen_optim` ";
+        $sql        .= "FROM `{$tableName}` ";
         $sql        .= "WHERE `id_fabrique_screen_optim` = '".$productId."' AND `id_curso_screen_optim` = ".$cursoId." AND `id_user_screen_optim` = ".$userId;
         $sql        .= " ORDER BY `id_screen_optim` DESC";
 
