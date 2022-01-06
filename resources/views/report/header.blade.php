38fc339f2356a4b8b09e719ad1a1fc69987e4a31
webdev02177 <huscartopdev@gmail.com>
2022-01-06 11:49:11 +0900

    fix: image and header inline block in model creating

--- resources/views/report/header.blade.php
+++ resources/views/report/header.blade.php
@@ -3,7 +3,7 @@
     <div style="border-top: 1px solid black; width: 100%;"></div>
 </div> -->
 
-<table style="width: 100%; position: relative; bottom: 0; left: 0; right: 0;" id="rep_header">
+<table style="width: 100%; position: relative; bottom: 0; left: 0; right: 0; display:inline-block;" id="rep_header">
     <tr>
         <td style="border-bottom: 1px solid black;">
             <b> Type here the text of the header that will be displayed on each page of the final report. You can add images, like your logo </b>
